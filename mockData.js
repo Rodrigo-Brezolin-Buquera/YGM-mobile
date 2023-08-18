f0cb262b2a9af23ec5085182aab5a56d52cdf2ce
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-18 13:47:31 -0300

    mocks

---
+++ mockData.js
@@ -0,0 +1,74 @@
+export const mockClasses = [
+    {
+        capacity: 16,
+        date: "18/08/2023",
+        day: "Terça",
+        groupId: "2023-06-16-17:56-Hatha",
+        id: "JYnMJsfqi8c9VF1fNcan",
+        name: "Hatha",
+        teacher: "Rodrigo",
+        time: "17:56"
+    },
+    {
+        capacity: 16,
+        date: "18/08/2023",
+        day: "Terça",
+        groupId: "2023-06-16-17:56-Hatha",
+        id: "JYnMJsfqi8c9VF1fNcan2",
+        name: "Hatha",
+        teacher: "Rodrigo",
+        time: "17:56"
+    }]
+
+export const mockContracts = [
+    {
+        availableClasses: null,
+        ends:  null,
+        id: "HfyQMsz00UbILrh9tPHzwytoRqc2",
+        name: "Rodrigo Brezolin",
+        plan: "Gympass",
+        started: "17/06/2023"
+    },
+    {
+        availableClasses: 2,
+        ends: "17/06/2023",
+        id: "MtblzYHeisbqkrOj2xG068be5wn22",
+        name: "Louize baya",
+        plan: "1X-Mensal",
+        started: "17/06/2023"
+    },
+    {
+        availableClasses: null,
+        ends: null,
+        id: "MtblzYHeisbqkrOj2xG068be5wn2222",
+        name: "Louize ",
+        plan: "Gympass",
+        started: "17/06/2023"
+    }
+
+]
+
+export const mockUsers = [
+    {
+        active: true,
+        admin: false,
+        email: "RODRIGOBBUQUERA@GMAIL.COM",
+        id: "HfyQMsz00UbILrh9tPHzwytoRqc2",
+        name: "Rodrigo Brezolin"
+    },
+    {
+        active: true,
+        admin: false,
+        email: "RODRIGOBBUQUERA@GMAIL.COM",
+        id: "MtblzYHeisbqkrOj2xG068be5wn22",
+        name: "Louize baya"
+    },
+    {
+        active: false,
+        admin: false,
+        email: "RODRIGOBBUQUERA@GMAIL.COM",
+        id: "MtblzYHeisbqkrOj2xG068be5wn2222",
+        name: "Louize"
+    }
+
+]
