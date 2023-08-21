cb629aa02f2a7e73cf7482bbe1774706df6d1231
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-21 19:17:47 -0300

    datePicker instalado

--- screens/Agenda/CreateClassModal.js/index.js
+++ screens/Agenda/CreateClassModal.js/index.js
@@ -1,16 +1,82 @@
-import React from 'react'
-import { Text, Modal } from 'react-native'
+import { useState } from 'react'
+import { Text, Modal, View, TouchableOpacity, TextInput, Button } from 'react-native'
+import globalStyles from '../../../globalStyles'
+import { getToday } from '../../../utils/dates';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
 
 const CreateClassModal = ({ isModalVisible, toggleModal, navigation }) => {
+  const [loading, setLoading] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [showTimePicker, setShowTimePicker] = useState(false);
+
+
+  const handleDateChange = (event, selected) => {
+    const currentDate = selected || selectedDate;
+    setShowDatePicker(false);
+    setSelectedDate(currentDate);
+  };
+
+  const handleTimeChange = (event, selected) => {
+    const currentTime = selected || selectedTime;
+    setShowTimePicker(false);
+    setSelectedTime(currentTime);
+  };
+
+
   return (
     <Modal
-    animationType="slide"
-    transparent={true}
-    visible={isModalVisible}
-    onRequestClose={toggleModal}
-> 
-<Text>Modal</Text>
-</Modal>
+      animationType="slide"
+      transparent={true}
+      visible={isModalVisible}
+      onRequestClose={toggleModal}
+    >
+      <TouchableOpacity
+        style={globalStyles.modalOverlay}
+        activeOpacity={1}
+        onPress={() => toggleModal(false)}
+      >
+        <View style={globalStyles.modalContainer}>
+          <View style={globalStyles.modalForm}>
+            <Text style={globalStyles.modalHeader}>Detalhes da aula</Text>
+
+            <Button title="Select Date" onPress={() => setShowDatePicker(true)} />
+            {showDatePicker && (
+              <DateTimePicker
+                value={selectedDate}
+                mode="date"
+                is24Hour={true}
+                display="default"
+                onChange={handleDateChange}
+              />
+            )}
+
+            <Button title="Select Time" onPress={() => setShowTimePicker(true)} />
+            {showTimePicker && (
+              <DateTimePicker
+                value={selectedTime}
+                mode="time"
+                is24Hour={true}
+                display="default"
+                onChange={handleTimeChange}
+              />
+            )}
+
+            <TouchableOpacity
+              style={globalStyles.button}
+              onPress={() => toggleModal(false)}
+              disabled={loading}
+            >
+              <Text style={globalStyles.buttonText}>Criar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </TouchableOpacity>
+    </Modal>
   )
 }
 
