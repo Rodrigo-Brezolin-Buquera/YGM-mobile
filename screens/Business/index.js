1a8def8601f0c10998b230b68acb27fb6299aad7
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-22 13:19:40 -0300

    renderização condicional tela espaço

--- screens/Business/index.js
+++ screens/Business/index.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import globalStyles from '../../globalStyles'
+import {useState} from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import globalStyles, { selectedButtonStyle } from '../../globalStyles'
 
 const Business = () => {
+  const [view, setView] = useState(null);
+
+  const BusinessView = () => {
+    switch (view) {
+      case 'local':
+        return  <Text>Local</Text>
+      case 'teacher/styles':
+        return <Text>teacher/styles</Text>
+        case 'plans':
+        return <Text>plans</Text>
+      default:
+        return <Text>nada</Text>
+    }
+  };
+
   return (
       <View style={globalStyles.container} >
-          <Text>Business</Text>
+        <View style={globalStyles.buttonContainer}>
+        <TouchableOpacity
+          style={[
+            globalStyles.button,
+            selectedButtonStyle(view === "local")
+          ]}
+          onPress={() => setView('local')}
+        >
+          <Text style={globalStyles.buttonText}  >Local</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+          style={[
+            globalStyles.button,
+            selectedButtonStyle(view === 'teacher/styles')
+          ]}
+          onPress={() => setView('teacher/styles')}
+        >
+          <Text style={globalStyles.buttonText}  >teacher/styles</Text>
+        </TouchableOpacity >
+
+        <TouchableOpacity
+          style={[
+            globalStyles.button,
+            selectedButtonStyle(view === "plans")
+          ]}
+          onPress={() => setView('plans')}
+        >
+          <Text style={globalStyles.buttonText}  >Planos</Text>
+        </TouchableOpacity>
+      </View>
+          <BusinessView/>
       </View>
   )
 }
