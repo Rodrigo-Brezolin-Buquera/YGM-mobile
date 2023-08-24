00f5ea6c827abd0e10d56c632caec2dc41f0480d
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-24 14:02:23 -0300

    classDetails

---
+++ screens/Class/ClassDetails/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        alignItems:"center",
+        width:"100%",
+        paddingTop:10,
+        paddingHorizontal:20
+    },
+    gap: {
+        marginBottom: 12,
+      }
+
+});
--- screens/Class/index.js
+++ screens/Class/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import {useState} from 'react'
 import { Text, View } from 'react-native'
 import globalStyles from '../../globalStyles'
+import {mockClasses} from "../../mockData"
+import ClassDetails from './ClassDetails'
 
 const Class = () => {
+  const [yogaClass, setYogaClass] = useState(mockClasses[0])
   return (
       <View style={globalStyles.container} >
-          <Text>Class</Text>
+          <ClassDetails yogaClass={yogaClass}/>
 
       </View>
   )
